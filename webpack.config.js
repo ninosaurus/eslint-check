5ae0f69f472b5c524636e76cccece2be7ed97d7e
Valentin Hisari <valentin.hisari@fishingbooker.com>
2019-12-02 22:29:51 +0100

    test

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = (env, argv) => {
+  return {
+    entry: {
+      index: './src/index.js'
+    },
+    target: 'node',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index.js'
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      modules: [path.resolve(__dirname, 'node_modules')]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              envName: argv.mode === 'production' ? 'production' : 'development'
+            }
+          }
+        }
+      ]
+    }
+  };
+};
