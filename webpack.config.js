b5dde809f86ad1736e01f3396f936cfd1dca00c7
Valentin Hisari <valentin.hisari@fishingbooker.com>
2019-12-03 00:02:22 +0100

    test

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,39 @@ const webpack = require('webpack');
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
-module.exports = (env, argv) => {
-  return {
-    entry: {
-      index: './src/index.js'
-    },
-    output: {
-      publicPath: './',
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'index.js'
-    },
-    target: 'node',
-    resolve: {
-      extensions: ['.js', '.jsx'],
-      modules: [path.resolve(__dirname, 'node_modules')]
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              babelrc: true
-            }
+module.exports = () => ({
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    publicPath: './',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  target: 'node',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: [path.resolve(__dirname, 'node_modules')]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: true
           }
         }
-      ]
-    },
-    externals: [
-      nodeExternals()
-    ],
-    plugins: [
-      new webpack.NamedModulesPlugin()
+      }
     ]
-  };
-};
+  },
+  externals: [
+    nodeExternals()
+  ],
+  plugins: [
+    new webpack.NamedModulesPlugin()
+  ]
+});
