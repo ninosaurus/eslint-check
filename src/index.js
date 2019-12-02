d9b31135cacb444aa2a74044109b08b5c9221cd1
Valentin Hisari <valentin.hisari@fishingbooker.com>
2019-12-02 14:38:06 +0100

    test

--- src/index.js
+++ src/index.js
@@ -68,9 +68,7 @@ async function createCheck() {
 
 function eslint(files) {
   const cli = new CLIEngine({
-    useEslintrc: false,
     extensions: ['.js', '.jsx', '.tsx'],
-    configFile: resolve(GITHUB_WORKSPACE, '.eslintrc'),
     cwd: GITHUB_WORKSPACE
   });
   console.log(process.cwd(), GITHUB_WORKSPACE);
