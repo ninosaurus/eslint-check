928ae57a4d8da23da5f8a565dfee8929047be008
Valentin Hisari <ninosaurus@users.noreply.github.com>
2019-12-04 15:01:09 +0100

    Bugfix (#16)
    * bugfix
    
    * bugfix

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,11 @@
 export const CHECK_NAME = 'ESLint check';
 
-export const EXTENSIONS_TO_LINT = new Set([
+export const EXTENSIONS_TO_LINT = [
   '.mjs',
   '.js',
   '.ts',
   '.jsx',
   '.tsx'
-]);
+];
 
 export const GITHUB_URL = 'github.com';
